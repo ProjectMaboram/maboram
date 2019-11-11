09c563652a6b83f81e2ae273d0de8243419d197d
Hang <HangTran@hotmail.se>
2019-11-11 15:15:33 +0100

    contact.js localstorage

--- js/contact.js
+++ js/contact.js
@@ -1,46 +1,43 @@
 const body = $("body");
 const formSection = $("#formContact");
 
-$(document).on('click', '#submit', () =>{
-    const firstName = $('#firstName').val();
-    const lastName = $('#lastName').val();
-    const address = $('#address').val();
-    const co = $('#co').val();
-    const zip = $('#zip').val();
-    const city = $('#city').val();
-   
+$(document).on("click", "#submit", () => {
+  const firstName = $("#firstName").val();
+  const lastName = $("#lastName").val();
+  const address = $("#address").val();
+  const co = $("#co").val();
+  const zip = $("#zip").val();
+  const city = $("#city").val();
 
-    const newInfo = $('<section>')
-    newInfo.append(`<p>Förnamn: ${firstName}</p>`)
-    newInfo.append(`<p>Efternamn: ${lastName}</p>`)
-    newInfo.append(`<p>Adress: ${address}</p>`);
-    newInfo.append(`<p>C/o: ${co}</p>`)
-    newInfo.append(`<p>Porst nr: ${zip}</p>`)
-    newInfo.append(`<p>Stad: ${city}</p>`)
-    
-    formSection.append(newInfo);
-    emptyContactForm();
+  const newInfo = $("<section>");
+  newInfo.append(`<p>Förnamn: ${firstName}</p>`);
+  newInfo.append(`<p>Efternamn: ${lastName}</p>`);
+  newInfo.append(`<p>Adress: ${address}</p>`);
+  newInfo.append(`<p>C/o: ${co}</p>`);
+  newInfo.append(`<p>Porst nr: ${zip}</p>`);
+  newInfo.append(`<p>Stad: ${city}</p>`);
+
+  formSection.append(newInfo);
+  emptyContactForm();
 });
 
 function emptyContactForm() {
-    $('#contactForm input').val('');
+  $("#contactForm input").val("");
 }
 $(document).ready(function() {
-    $("#submit").click(function () {
-        $("#contactForm").toggle();
-        $("#confirm").toggle();
-        $("#submit").toggle();
-        $("#cancel").toggle();
-    });
+  $("#submit").click(function() {
+    $("#contactForm").toggle();
+    $("#confirm").toggle();
+    $("#submit").toggle();
+    $("#cancel").toggle();
+  });
 });
 
-$(document).ready(function () {
-    $("#cancel").click(function () {
-      emptyContactForm()
-    })
+$(document).ready(function() {
+  $("#cancel").click(function() {
+    emptyContactForm();
+  });
 });
-
-
 
 /* Localstorage */
 function getFirstName() {
@@ -69,15 +66,12 @@ function getCity() {
   return input.value;
 }
 function loadNames() {
-  const firstName = localStorage.getItem("first");
-  const lastName = localStorage.getItem("last");
-  const address = localStorage.getItem("address");
-  const co = localStorage.getItem("co");
-  const zip = localStorage.getItem("zip");
-  const city = localStorage.getItem("city");
+  const { firstname, lastname, address, co, zip, city } = localStorage.getItem(
+    "user"
+  );
 
-  document.querySelector("#firstName").value = firstName;
-  document.querySelector("#lastName").value = lastName;
+  document.querySelector("#firstName").value = firstname;
+  document.querySelector("#lastName").value = lastname;
   document.querySelector("#address").value = address;
   document.querySelector("#co").value = co;
   document.querySelector("#zip").value = zip;
@@ -89,13 +83,16 @@ loadNames();
 const button = document.querySelector("#submit");
 
 button.addEventListener("click", () => {
-  localStorage.setItem("first", getFirstName());
-  localStorage.setItem("last", getLastName());
-  localStorage.setItem("address", getAddress());
-  localStorage.setItem("co", getCo());
-  localStorage.setItem("zip", getZip());
-  localStorage.setItem("city", getCity());
-});   /* /Localstorage */
+  const details = {
+    firstname: getFirstName(),
+    lastname: getLastName(),
+    address: getAddress(),
+    co: getCo(),
+    zip: getZip(),
+    city: getCity()
+  };
+  localStorage.setItem("user", details);
+}); /* /Localstorage */
 // Antal varor utskrivet på kundvagns-ikon
 const cartManager = new CartManager();
 $(".badge").text(cartManager.getNumberOfItems());
