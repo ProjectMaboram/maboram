7e8f2269d87acd7718131b31fd1f0a072a0bcc45
Hang <HangTran@hotmail.se>
2019-11-11 08:55:37 +0100

    index

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,5 @@
-const body = $('body')
-const formSection = $('#formContact')
-
+const body = $("body");
+const formSection = $("#formContact");
 
 $(document).on('click', '#submit', () =>{
     const firstName = $('#firstName').val();
@@ -41,3 +40,56 @@ $(document).ready(function () {
     })
 });
 
+function getFirstName() {
+  const input = document.querySelector("#firstName");
+  return input.value;
+}
+
+function getLastName() {
+  const input = document.querySelector("#lastName");
+  return input.value;
+}
+function getAddress() {
+  const input = document.querySelector("#address");
+  return input.value;
+}
+function getCo() {
+  const input = document.querySelector("#co");
+  return input.value;
+}
+function getZip() {
+  const input = document.querySelector("#zip");
+  return input.value;
+}
+function getCity() {
+  const input = document.querySelector("#city");
+  return input.value;
+}
+function loadNames() {
+  const firstName = localStorage.getItem("first");
+  const lastName = localStorage.getItem("last");
+  const address = localStorage.getItem("address");
+  const co = localStorage.getItem("co");
+  const zip = localStorage.getItem("zip");
+  const city = localStorage.getItem("city");
+
+  document.querySelector("#firstName").value = firstName;
+  document.querySelector("#lastName").value = lastName;
+  document.querySelector("#address").value = address;
+  document.querySelector("#co").value = co;
+  document.querySelector("#zip").value = zip;
+  document.querySelector("#city").value = city;
+}
+
+loadNames();
+
+const button = document.querySelector("#submit");
+
+button.addEventListener("click", () => {
+  localStorage.setItem("first", getFirstName());
+  localStorage.setItem("last", getLastName());
+  localStorage.setItem("address", getAddress());
+  localStorage.setItem("co", getCo());
+  localStorage.setItem("zip", getZip());
+  localStorage.setItem("city", getCity());
+});
