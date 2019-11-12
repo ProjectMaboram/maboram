f9e82002b6b73f8d3e10a8e5fb0b8365dbc6b252
Eric Lennox <itsrvn.dev@gmail.com>
2019-11-12 15:12:04 +0100

    add custom frame to cart

--- js/design.js
+++ js/design.js
@@ -1,14 +1,51 @@
 const carouselPrev = id => $(id).carousel("prev");
 const carouselNext = id => $(id).carousel("next");
 
-function getCurrentFrame(id) {
-  return $("#carousel-top")[0].getElementsByClassName("carousel-inner");
-}
+let frameTop = "straight",
+  frameBot = "invard",
+  frameRight = "spikes",
+  frameLeft = "bubble";
 
 $(document).ready(() => {
   $(".carousel").carousel("pause");
 });
 
 $("#btn-next").click(() => {
-  console.log(getCurrentFrame("#carousel-top"));
+  let itemText = `FR: ${frameRight}, FL: ${frameLeft}, FT: ${frameTop}, FB: ${frameBot}`;
+  let item = new CartItem("egen-designad-ram : " + itemText, 1, 850);
+  cartManager.add(item);
+  window.location.href = "shopping-cart.html";
+});
+
+const getFrameName = idx => {
+  switch (idx) {
+    case 0:
+      return "spikes";
+      break;
+    case 1:
+      return "bubble";
+      break;
+    case 2:
+      return "straight";
+      break;
+    case 3:
+      return "invard";
+      break;
+  }
+};
+
+$("#carousel-top").on("slid.bs.carousel", props => {
+  frameTop = getFrameName(props.from);
+});
+
+$("#carousel-bot").on("slid.bs.carousel", props => {
+  frameBot = getFrameName(props.from);
+});
+
+$("#carousel-left").on("slid.bs.carousel", props => {
+  frameLeft = getFrameName(props.from);
+});
+
+$("#carousel-right").on("slid.bs.carousel", props => {
+  frameRight = getFrameName(props.from);
 });
